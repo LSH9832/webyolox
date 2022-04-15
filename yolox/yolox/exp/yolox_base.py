90046b37b289025974256a8089d332abcc025635
Shihan Liu <69880398+LSH9832@users.noreply.github.com>
2022-04-15 17:16:46 +0800

    Update yolox_base.py

--- yolox/yolox/exp/yolox_base.py
+++ yolox/yolox/exp/yolox_base.py
@@ -100,79 +100,37 @@ class Exp(BaseExp):
         self.model.head.initialize_biases(1e-2)
         return self.model
 
-    def get_data_loader(
-        self, batch_size, is_distributed, no_aug=False, cache_img=False
-    ):
-        from yolox.data import (
-            COCODataset,
-            TrainTransform,
-            YoloBatchSampler,
-            DataLoader,
-            InfiniteSampler,
-            MosaicDetection,
-            worker_init_reset_seed,
-        )
-        from yolox.utils import (
-            wait_for_the_master,
-            get_local_rank,
-        )
+    def get_data_loader(self, batch_size, is_distributed, no_aug=False, cache_img=False):
+
+        from yolox.data import get_dataloader
+        from yolox.utils import wait_for_the_master, get_local_rank
 
         local_rank = get_local_rank()
-
-        with wait_for_the_master(local_rank):
-            dataset = COCODataset(
-                data_dir=self.data_dir,
-                json_file=self.train_ann,
-                img_size=self.input_size,
-                train_dir=self.train_dir,
-                preproc=TrainTransform(
-                    max_labels=50,
-                    flip_prob=self.flip_prob,
-                    hsv_prob=self.hsv_prob
-                ),
-                cache=cache_img,
-            )
-
-        dataset = MosaicDetection(
-            dataset,
-            mosaic=not no_aug,
-            img_size=self.input_size,
-            preproc=TrainTransform(
-                max_labels=120,
-                flip_prob=self.flip_prob,
-                hsv_prob=self.hsv_prob),
-            degrees=self.degrees,
-            translate=self.translate,
-            mosaic_scale=self.mosaic_scale,
-            mixup_scale=self.mixup_scale,
-            shear=self.shear,
-            enable_mixup=self.enable_mixup,
-            mosaic_prob=self.mosaic_prob,
-            mixup_prob=self.mixup_prob,
-        )
-
-        self.dataset = dataset
-
         if is_distributed:
             batch_size = batch_size // dist.get_world_size()
 
-        sampler = InfiniteSampler(len(self.dataset), seed=self.seed if self.seed else 0)
-
-        batch_sampler = YoloBatchSampler(
-            sampler=sampler,
-            batch_size=batch_size,
-            drop_last=False,
-            mosaic=not no_aug,
-        )
-
-        dataloader_kwargs = {"num_workers": self.data_num_workers, "pin_memory": True}
-        dataloader_kwargs["batch_sampler"] = batch_sampler
-
-        # Make sure each process has different random seed, especially for 'fork' method.
-        # Check https://github.com/pytorch/pytorch/issues/63311 for more details.
-        dataloader_kwargs["worker_init_fn"] = worker_init_reset_seed
-
-        train_loader = DataLoader(self.dataset, **dataloader_kwargs)
+        with wait_for_the_master(local_rank):
+            train_loader, self.dataset = get_dataloader(
+                batch_size=batch_size,
+                data_dir=self.data_dir,
+                json_file=self.train_ann,
+                train_dir=self.train_dir,
+                input_size=self.input_size,
+                no_aug=no_aug,
+                cache_img=cache_img,
+                flip_prob=self.flip_prob,
+                hsv_prob=self.hsv_prob,
+                degrees=self.degrees,
+                translate=self.translate,
+                mosaic_scale=self.mosaic_scale,
+                mixup_scale=self.mixup_scale,
+                shear=self.shear,
+                enable_mixup=self.enable_mixup,
+                mosaic_prob=self.mosaic_prob,
+                mixup_prob=self.mixup_prob,
+                data_num_workers=self.data_num_workers,
+                seed=self.seed
+            )
 
         return train_loader
 
