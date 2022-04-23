54e2bd741cf99698a60015fd583dcc4b71637a1d
Shihan Liu <69880398+LSH9832@users.noreply.github.com>
2022-04-24 04:01:36 +0800

    Update __init__.py

--- yolox/yolox/models/__init__.py
+++ yolox/yolox/models/__init__.py
@@ -5,7 +5,7 @@ from .yolo_head import YOLOXHead
 from .yolo_pafpn import YOLOPAFPN, BACKBONE
 from .yolox import YOLOX
 
-def get_model(depth, width, num_classes=80, in_channels=[256, 512, 1024], act="silu", backbone_type="origin"):
-    backbone = BACKBONE[backbone_type](depth, width, in_channels=in_channels, act=act)
+def get_model(depth, width, num_classes=80, in_channels=[256, 512, 1024], act="silu", backbone_type="origin", depthwise=False):
+    backbone = BACKBONE[backbone_type](depth, width, in_channels=in_channels, act=act, depthwise=depthwise)
     head = YOLOXHead(num_classes, width, in_channels=in_channels, act=act)
     return YOLOX(backbone, head)
